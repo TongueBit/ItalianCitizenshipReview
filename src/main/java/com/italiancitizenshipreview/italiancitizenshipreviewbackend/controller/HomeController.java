d1dbbe3814e7ff41af6884a0679b67f1c7d9d671
Tony Edmonds <Tonyledmonds@gmail.com>
2023-05-06 12:28:21 +0200

    fixed the controller for / to be also give view for home

--- src/main/java/com/italiancitizenshipreview/italiancitizenshipreviewbackend/controller/HomeController.java
+++ src/main/java/com/italiancitizenshipreview/italiancitizenshipreviewbackend/controller/HomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HomeController {
 
+
+    @GetMapping("/")
+    public String homePageRedirect() {
+        return "home";
+    }
     @GetMapping("/home")
     public String homePage() {
         return "home";
